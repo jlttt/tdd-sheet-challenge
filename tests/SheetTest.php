5e7156a956e46a7c58c25f8285533bb22abef5af
jean-luc <jean-luc@pmpconcept.com>
2017-04-28 07:27:30 +0200

    phpunit backward compatibility

--- tests/SheetTest.php
+++ tests/SheetTest.php
@@ -1,6 +1,12 @@
 <?php
 require_once __DIR__ . '/../src/Sheet.php';
 
+// backward compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase') &&
+    class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+
 class SheetTest extends PHPUnit_Framework_TestCase
 {
     protected $sheet;
